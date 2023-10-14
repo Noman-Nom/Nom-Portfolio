9b7fcb62d9d55162d8ce00bb8bd49c230ae6e141
Noman <noman5456318@gmail.com>
2023-10-14 12:37:52 +0500

    folder structure

--- nom-portfolio/src/App.jsx
+++ nom-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import { About,Footer,Work,Testimonials,Skills,Header } from './Container'
+import {Navbar} from './Components'
+
 
 const App = () => {
   return (
     <div className='app'>
-
+      <Navbar/>
       <Header/>
       <About/>
       <Work/>
