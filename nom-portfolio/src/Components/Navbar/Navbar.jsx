9addce527ffd8fde87b51072fa5f8a384f20a1b2
Noman <noman5456318@gmail.com>
2023-10-15 13:49:07 +0500

    mobile responsive nav working

--- nom-portfolio/src/Components/Navbar/Navbar.jsx
+++ nom-portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,52 +1,50 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
-import {BsFillMenuButtonWideFill} from 'react-icons/bs'
-import {motion} from 'framer-motion'
+import { BsFillMenuButtonWideFill } from "react-icons/bs";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { motion } from "framer-motion";
 
-import { images } from '../../Constants'
+import { images } from "../../Constants";
 
-import './Navbar.scss'
+import "./Navbar.scss";
 
 const Navbar = () => {
-
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
   return (
-    
-   <nav className='app__navbar'>
-    <div className='app__navbar-logo'>
-      <img src={images.nomLogo2} alt="my-logo" />
-    </div>
-    <ul className='app__navbar-links'>
-      {
-        ['Home','About', 'Work', 'Skills','Contacts'].map((item)=>(
-
-              <li className='app__flex p-text' key={`link-${item}`}>
-                <a href={`#${item}`}>{item}</a>
-                <div/>
-              </li>
-        ))
-      }
-    </ul>
-    <div className="app__navbar-menu">
-      <BsFillMenuButtonWideFill onClick={()=>setToggle(true)}/>
-      {
-        toggle && ( 
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.nomLogo2} alt="my-logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["Home", "About", "Work", "Skills", "Contacts"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <a href={`#${item}`}>{item}</a>
+            <div />
+          </li>
+        ))}
+      </ul>
+      <div className="app__navbar-menu">
+        <BsFillMenuButtonWideFill onClick={() => setToggle(true)} />
+        {toggle && (
           <motion.div
-          
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
           >
+            <AiOutlineCloseCircle onClick={() => setToggle(false)} />
 
-
+            {["Home", "About", "Work", "Skills", "Contacts"].map((item) => (
+          <li  key={item}>
+            <a href={`#${item}`} onClick={() => setToggle(false)} >{item}</a>
+          
+          </li>
+        ))}
+           
           </motion.div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-        )
-      }
-
-    </div>
-   </nav>
-
-  
-  )
-}
-
-export default Navbar
+export default Navbar;
