d4c190bd7dd4234c7435db2657ff934c12edd0c4
Noman <noman5456318@gmail.com>
2023-10-17 01:21:29 +0500

    header section working

--- nom-portfolio/src/Container/Header/Header.jsx
+++ nom-portfolio/src/Container/Header/Header.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import {motion } from 'framer-motion'
+import React from "react";
+import { motion } from "framer-motion";
 
-import { images } from '../../Constants'
-import './Header.scss'
+import { images } from "../../Constants";
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
-
+    <div className="app__header app__flex">
       <motion.div
-      whileInView={{x:[-100,0], opacity:[0,1]}}
-      transition={{duration:0.5}}
-
-      className='app__header-info'
-
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
       >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{marginLeft:20}}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Noman</h1>
 
+            </div>
+          </div>
+        </div>
       </motion.div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
