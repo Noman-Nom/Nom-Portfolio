5dda8c905274377d9cb8ad2b5545809ce11305ba
Noman <noman5456318@gmail.com>
2023-10-18 01:08:19 +0500

    wraper section started

---
+++ nom-portfolio/src/Components/SocialMedia.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import {AiFillGithub , AiFillFacebook, AiFillLinkedin,AiFillTwitterCircle, AiOutlineInstagram} from 'react-icons/ai'
+
+
+const SocialMedia = () => {
+  return (
+    <div>SocialMedia</div>
+  )
+}
+
+export default SocialMedia
