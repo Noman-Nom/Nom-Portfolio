cd77eaf4ed045b55c3e6b9d8c08aab44217cc9ef
Noman <noman5456318@gmail.com>
2023-10-17 19:03:58 +0500

    about section started

--- nom-portfolio/src/Container/About/About.jsx
+++ nom-portfolio/src/Container/About/About.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+import {motion} from 'framer-motion'
 import './About.scss'
 const About = () => {
   return (
-    <div>About</div>
+   <>
+   <h2 className='head-text'> "Where <span>Good Design</span> 
+   <br />Meets Great <span>Business Outcomes</span>"</h2>
+   </>
   )
 }
 
