8b902279800f9f7332bf3b5f718e6a4a65607cba
Noman <noman5456318@gmail.com>
2023-10-18 02:44:05 +0500

    work section started

--- nom-portfolio/src/Container/Work/Work.jsx
+++ nom-portfolio/src/Container/Work/Work.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import { useState, useEffect } from 'react'
+import {motion} from 'framer-motion'
+import {AppWrap} from '../../Wrapper'
+import {urlFor , Client} from '../../Client'
+
 import './Work.scss'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+    <h2 className='head-text'>My <span>Full Stack</span> Web Development <br /> <span>Portfolio</span> Section </h2>
+    </>
   )
 }
 
