8d828e6a2ad2739271a8f50d63b825dc8e8c21be
Noman <noman5456318@gmail.com>
2023-10-18 23:21:37 +0500

    skills and expeerience sectio  started

--- nom-portfolio/src/Container/Skills/Skills.jsx
+++ nom-portfolio/src/Container/Skills/Skills.jsx
@@ -40,7 +40,21 @@ const Skills = () => {
       <motion.div
       className='app__skills-list'
       >
-       
+       {skills.map((skill)=>(
+          <motion.div
+          whileInView={{opacity:[0,1]}}
+          transition={{duration:0.5}}
+          className='app__skills app__flex'
+
+          key={skill.name}
+          
+          >
+              <div className='app__flex' style={{backgroundColor:skill.bgColor}} >
+                  <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className='p-text'>{skill.name}</p>
+          </motion.div>
+       ))}
       </motion.div>
 
     </div>
