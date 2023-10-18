3deef18c11867bdf8d44391f924235b18c7d39da
Noman <noman5456318@gmail.com>
2023-10-18 23:40:30 +0500

    skills and expeerience sectio  started

--- nom-portfolio/src/Container/Skills/Skills.jsx
+++ nom-portfolio/src/Container/Skills/Skills.jsx
@@ -44,7 +44,7 @@ const Skills = () => {
           <motion.div
           whileInView={{opacity:[0,1]}}
           transition={{duration:0.5}}
-          className='app__skills app__flex'
+          className='app__skills-item app__flex'
 
           key={skill.name}
           
