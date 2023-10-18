48ad7562277e3440af9644ba0c727b9447d25a22
Noman <noman5456318@gmail.com>
2023-10-18 23:13:53 +0500

    skills and expeerience sectio  started

--- nom-portfolio/src/Container/Skills/Skills.jsx
+++ nom-portfolio/src/Container/Skills/Skills.jsx
@@ -8,6 +8,29 @@ import { urlFor, Client } from "../../Client";
 import './Skills.scss'
 
 const Skills = () => {
+
+  const [experiences, setExperiences] = useState([])
+  const [skills, setSkills] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    Client.fetch(query)
+      .then((data) => {
+        setExperiences(data);
+       
+      })
+    Client.fetch(skillsQuery)
+      .then((data) => {
+        setSkills(data);
+       
+      })
+      .catch((error) =>
+        console.error("Error fetching data from Sanity:", error)
+      );
+  }, []);
+
   return (
     <>
     <h2 className='head-text'>
