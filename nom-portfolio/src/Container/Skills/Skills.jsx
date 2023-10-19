5be8be4ac2596f8f9fd9f3dee958428c9a6de3a7
Noman <noman5456318@gmail.com>
2023-10-20 01:14:25 +0500

    skills section working

--- nom-portfolio/src/Container/Skills/Skills.jsx
+++ nom-portfolio/src/Container/Skills/Skills.jsx
@@ -1,32 +1,26 @@
-import { Tooltip as ReactTooltip } from 'react-tooltip'
+import { Tooltip as ReactTooltip } from "react-tooltip";
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { AppWrap } from "../../Wrapper";
 import { urlFor, Client } from "../../Client";
 
-
-import './Skills.scss'
+import "./Skills.scss";
 
 const Skills = () => {
-
-  const [experiences, setExperiences] = useState([])
-  const [skills, setSkills] = useState([])
+  const [experiences, setExperiences] = useState([]);
+  const [skills, setSkills] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "experiences"]';
     const skillsQuery = '*[_type == "skills"]';
 
-    Client.fetch(query)
-      .then((data) => {
-
-        console.log(data)
-        setExperiences(data);
-       
-      })
+    Client.fetch(query).then((data) => {
+      console.log(data);
+      setExperiences(data);
+    });
     Client.fetch(skillsQuery)
       .then((data) => {
         setSkills(data);
-       
       })
       .catch((error) =>
         console.error("Error fetching data from Sanity:", error)
@@ -35,51 +29,73 @@ const Skills = () => {
 
   return (
     <>
-    <h2 className='head-text'>
-      Skills <span>&</span> Experience
-    </h2>
-    <div className='app__skills-container'>
-      <motion.div
-      className='app__skills-list'
-      >
-       {skills.map((skill)=>(
-          <motion.div
-          whileInView={{opacity:[0,1]}}
-          transition={{duration:0.5}}
-          className='app__skills-item app__flex'
-
-          key={skill.name}
-          
-          >
-              <div className='app__flex' style={{backgroundColor:skill.bgColor}} >
-                  <img src={urlFor(skill.icon)} alt={skill.name} />
-              </div>
-              <p className='p-text'>{skill.name}</p>
-          </motion.div>
-       ))}
-      </motion.div>
-
-      <motion.div className='app__skills-exp' >
-
-       
-          {
-            experiences?.map((experience)=>(
+      <h2 className="head-text">
+        Skills <span>&</span> Experience
+      </h2>
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
             <motion.div
-            className='app__skills-exp-item'
-            key={experience.year}
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
             >
-                  {/* {console.log("jasj",experience.year)} */}
-                <div className='app__skills-exp-year'>
-                    <p className='bold-text'>{experience.year}</p>
-                </div>
-
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
             </motion.div>
-            )) }
-      </motion.div>
+          ))}
+        </motion.div>
 
-    </div>
+        <motion.div className="app__skills-exp">
+          {experiences?.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
+              {/* {console.log("jasj",experience.year)} */}
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+
+              <motion.div className="app__skills-exp-works">
+                
+                {experience.works.map((work)=>(
+                       <>
+              <motion.div
+               whileInView={{opacity:[0,1]}}
+               transition={{duration:0.5}}
+               className='app__skills-exp-work'
+
+             
+               data-for={work.name}
+     
+               key={work.name}
+              >
+                  <h4 className='bold-text'>{work.name}</h4>
+                  <p className='p-text'>{work.company}</p>
+              </motion.div>
+
+              <ReactTooltip
+            effect = "solid"
+            id={work.name}
+            arrowColor='#fff'
+            className='skills-tooltip'
+              >
+                  {work.desc}
+              </ReactTooltip>
+              </>
+                ))}
+                
+                </motion.div>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
