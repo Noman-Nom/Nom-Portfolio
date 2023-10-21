973ef4c872d2fccdbe8070e94d2af61146442912
Noman <noman5456318@gmail.com>
2023-10-22 02:06:16 +0500

    footer section started

--- nom-portfolio/src/Container/Footer/Footer.jsx
+++ nom-portfolio/src/Container/Footer/Footer.jsx
@@ -19,6 +19,31 @@ const Footer = () => {
           <a href="tel:+92-3045456318" className="p-text">Contact Me via Phone no</a>
         </div>
       </div>
+
+
+      <div className="app__footer-form app__flex">
+
+        <div className="app__flex">
+
+          <input className="p-text" type="text" placeholder="Your Name"  name="name" value={name} onChange={handleChangeInput}  />
+
+        </div>
+        <div className="app__flex">
+
+          <input className="p-text" type="text" placeholder="Your Email"  name="email" value={email} onChange={handleChangeInput}  />
+
+        </div>
+        <div>
+          <textarea className="p-text" placeholder="Your Message" 
+          value={message}
+          name="message"
+          
+          onChange={handleChangeInput}/>
+        </div>
+        <button type="button" className="p-text" onClick={handleChangeInput}>
+
+        </button>
+      </div>
     </>
   );
 };
