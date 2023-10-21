6cf71e3e7a9f3b8a94cfc8bb1deed7a625bc9c8c
Noman <noman5456318@gmail.com>
2023-10-22 01:33:54 +0500

    footer section started

--- nom-portfolio/src/Container/Footer/Footer.jsx
+++ nom-portfolio/src/Container/Footer/Footer.jsx
@@ -12,7 +12,11 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card">
           <img src={images.email} alt="email" />
-          <a href="mailto:noman5456318@gmail.com" className="p-tet"></a>
+          <a href="mailto:noman5456318@gmail.com" className="p-text">Contact Me via Email</a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="email" />
+          <a href="tel:+92-3045456318" className="p-text">Contact Me via Phone no</a>
         </div>
       </div>
     </>
